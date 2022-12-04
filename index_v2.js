91edab4e56d69241ef73ab7848dc5eea1f87a4ee
Marat KH <336199@niuitmo.ru>
2022-12-05 02:11:13 +0300

    qqq

---
+++ index_v2.js
@@ -0,0 +1,53 @@
+import { Server } from 'http';
+import path from 'path';
+import fs from 'fs';
+
+const __dirname = path.resolve();
+
+// CORS headers
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
+
+// HttpServer PORT
+const PORT = process.env.PORT || 8000;
+
+const server = Server((req, res) => {
+    
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', ...CORS });
+        res.write('<h1>Да</h1>\n');
+        res.write(JSON.stringify(req.headers));
+    } else if (req.url === '/index.html') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), function (err, html) {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', ...CORS });
+            res.end(html);
+        });       
+    } else if (req.url === '/download') {
+        res.writeHead(200, {'Content-Disposition': 'attachment; filename="File.txt"'});
+        res.write('File\n');
+    } else if (req.url.startsWith('/timer')) {
+        const secs = req.url.substring(1 + req.url.indexOf('?'));
+        return setTimeout(() => res.end(`<h1>Miliseconds: ${secs}ms</h1>\n`), secs);
+    } else if (req.url.startsWith('/blocking')) {
+        const secs = req.url.substring(1 + req.url.indexOf('?'));
+        // blocking thread code
+        ((y = 10000, x = Date.now()) => { while (Date.now() - x < y) ;})();
+        return setTimeout(() => res.end(`<h1>Miliseconds: ${secs}ms</h1>\n`), secs);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8', ...CORS });
+        res.write('Not Found\n');
+    }
+
+    res.end();
+
+});
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
---
+++ index_v3.js
@@ -0,0 +1,51 @@
+import { Server } from 'http';
+import finalhandler from 'finalhandler';
+import path from 'path';
+import { createReadStream as createRS } from 'fs';
+import serveStatic from 'serve-static';
+
+// __DIRNAME
+const __dirname = path.resolve();
+
+// use current directory
+var serve = serveStatic('.');
+
+// CORS headers
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
+
+// HttpServer PORT
+const PORT = process.env.PORT || 8000;
+
+const server = Server((req, res) => {
+    
+    if (req.url.startsWith('/')) {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', ...CORS });
+        res.write('<h1>Да</h1>\n');
+        res.write(JSON.stringify(req.headers));
+    } else if (req.url.startsWith('/index.html') || req.url === '/') {
+        // reading file using read stream
+        return createRS(path.join(__dirname, 'public', 'index.html')).pipe(res);
+    } else if (req.url.startsWith('/download')) {
+        res.writeHead(200, {'Content-Disposition': 'attachment; filename="File.txt"'});
+        res.write('File\n');
+    } else if (req.url.startsWith('/timer')) {
+        const secs = req.url.substring(1 + req.url.indexOf('?'));
+        return setTimeout(() => res.end(`<h1>Miliseconds: ${secs}ms</h1>\n`), secs);
+    } else if (req.url.startsWith('/blocking')) {
+        const secs = req.url.substring(1 + req.url.indexOf('?'));
+        // blocking thread code
+        ((y = 10000, x = Date.now()) => { while (Date.now() - x < y) ;})();
+        return setTimeout(() => res.end(`<h1>Miliseconds: ${secs}ms</h1>\n`), secs);
+    } else {
+        return serve(req, res, finalhandler(req, res))
+    }
+
+    res.end();
+
+});
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
